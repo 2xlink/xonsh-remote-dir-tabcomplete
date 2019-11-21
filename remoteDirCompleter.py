d46059c7a14e18700e84685fac3266e2ef31b763
2xlink <2xlink@web.de>
2019-11-21 11:26:40 +0100

    Adds script

---
+++ remoteDirCompleter.py
@@ -0,0 +1,13 @@
+def remoteDirCompleter(prefix, line, begindx, endidx, ctx):
+    import re
+    result = re.findall("[A-Za-z]+:[A-Za-z0-9/]*", prefix)
+
+    if result: # is not empty
+        hostName, path = result[0].split(":")
+        if not path: # is empty
+            path = "/"
+
+        fileList = re.split("\n", $(ssh @(hostName) find @(path)* -maxdepth 0))
+
+        return set(fileList)
+    return None
