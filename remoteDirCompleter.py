9f635495c8ecb211add0d702e98a87223a153940
2xlink <2xlink@web.de>
2019-11-22 17:38:07 +0100

    Adds trailing slashes to dirs

--- remoteDirCompleter.py
+++ remoteDirCompleter.py
@@ -1,17 +1,18 @@
 def remoteDirCompleter(prefix, line, begindx, endidx, ctx):
-    import re
+    import re, subprocess
     result = re.findall("[A-Za-z]+:[^:]*", prefix)
 
     if result: # is not empty
         hostName, path = result[0].split(":")
         if not path: # is empty
-            path = "/"
+            path = ""
 
-        fileList = re.split("\n", $(ssh @(hostName) find @(path)* -maxdepth 0 ))
+        fileList = re.split("\n", $(ssh @(hostName) ls -d1FL @(path + "*")))
 
         fileList = filter(lambda x: x != '', fileList)
 
         fileList = map(lambda x: hostName + ":" + x, fileList)
 
         return set(fileList)
+
     return None
